318f0d03f912354a74b034d2de57b174bcb3554e
istvank <koren@dbis.rwth-aachen.de>
2019-01-17 09:33:49 +0100

    Updated lit-element dependency

--- openidconnect-signin.js
+++ openidconnect-signin.js
@@ -4,7 +4,7 @@ Copyright (c) 2018 Advanced Community Information Systems (ACIS) Group, Chair of
 Information Systems), RWTH Aachen University, Germany. All rights reserved.
 */
 
-import {LitElement, html} from '@polymer/lit-element';
+import {LitElement, html} from 'lit-element';
 
 import {openidconnectIcon, signOutIcon} from './openidconnect-icons.js';
 
