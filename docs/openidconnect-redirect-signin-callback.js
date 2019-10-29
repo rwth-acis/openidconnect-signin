5dd4a18fb9f0bfb39273235aa90bf444ae57434b
Michi <michael.kretschmer@rwth-aachen.de>
2019-10-29 17:55:34 +0100

    fixed lit-element import

--- docs/openidconnect-redirect-signin-callback.js
+++ docs/openidconnect-redirect-signin-callback.js
@@ -4,9 +4,8 @@ Copyright (c) 2018 Advanced Community Information Systems (ACIS) Group, Chair of
 Information Systems), RWTH Aachen University, Germany. All rights reserved.
 */
 
-import {LitElement} from 'lit-element';
-
-import 'oidc-client';
+import { LitElement } from "./node_modules/lit-element/lit-element.js";
+import "./node_modules/oidc-client/lib/oidc-client.min.js";
 
 class OpenIDConnectRedirectSigninCallback extends LitElement {
 
